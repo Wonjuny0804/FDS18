0108b4aa22d88f7da83e5d1049528dee618fcdaa
WonjunJang <wonjun84@naver.com>
2020-11-01 08:48:06 +0900

    layout design

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let section = $('.board section');
+var tab = $('.board .tab a');
+var item = $('.board li');
+
+item.addClass('icon-dot-circled');
+
+tab.click(function name(e){
+    e.preventDefault();
+});
+
+tab.click(function(e) {
+    e.preventDefault();
+    section.removeClass('is-active');
+    $(this).parent().parent().addClass('is-active');
+})
